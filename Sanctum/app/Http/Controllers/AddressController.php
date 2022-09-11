804b23c831d2494af43b08a4b01cc859724b39b1
tahmid12345 <tahmid@inflexionpointbd.com>
2022-09-11 23:07:15 +0600

    Dynamic api system build up progress

---
+++ Sanctum/app/Http/Controllers/AddressController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Address;
+use Illuminate\Http\Request;
+
+class AddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $addresses = Address::all();
+        return response()->json($addresses);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $inputs = \request()->validate([
+            'title' => 'required',
+            'content' => 'required',
+            'image' => 'required|mimes:jpeg,jpg,png',
+        ]);
+//
+        if (request('image')) {
+            $inputs['image'] = \request('image')->store('images');
+        }
+
+        $address = Address::create($inputs);
+        return response()->json($inputs);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Address $address)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Address $address)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Address $address)
+    {
+        $inputs = \request()->validate([
+            'title' => 'required',
+            'content' => 'required',
+            'image' => 'required|mimes:jpeg,jpg,png',
+        ]);
+
+        if (request('image')) {
+            $inputs['image'] = \request('image')->store('images');
+        }else {
+            $inputs['image'] = $address->image;
+        }
+
+        $address = $address->update($inputs);
+        return response()->json($address);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Address $address)
+    {
+        $address->delete();
+
+        return response()->json("deleted");
+
+    }
+}
---
+++ Sanctum/app/Models/Address.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function getImageAttribute($value){
+        return asset('storage/'.$value);
+    }
+}
